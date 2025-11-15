4c4281aa3e6fa6122ad456461393fe322b356aa4
Дмитрий Жданов <D.Zhdanov66@ya.ru>
2025-11-15 21:54:05 +0300

    practice 7

--- practice/src/Main.java
+++ practice/src/Main.java
@@ -1,4 +1,17 @@
 
 void main() {
-
+    Scanner console = new Scanner(System.in);
+    int a = console.nextInt();
+    int i = (a == 1) ? 1 : (a == 2) ? 2 : 3;
+    /*Аналогично:
+    int i = 0;
+    if (a == 1) {3
+        i = 1;
+    } else if (a == 2) {
+        i = 2;
+    } else {
+        i = 3;
+    }
+     */
+    System.out.println(i);
 }
