9269c6fa947746ada105787f1245a9d3dff0a7d8
Дмитрий Жданов <D.Zhdanov66@ya.ru>
2025-11-17 15:27:09 +0300

    practice 13

--- practice/src/Main.java
+++ practice/src/Main.java
@@ -1,4 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        //через имя класса (рекомендуемый способ для static-методов)
+        A.printVars();
 
-void main() {
+        //через null-ссылку (антипаттерн)
+        A obj1 = null;
+        obj1.printVars();
 
+        //через не-null объект (тоже антипаттерн)
+        A obj2 = new A();
+        obj2.printVars();
+    }
+}
+
+class A {
+    public static int a = 1;
+    public static int b;
+
+    public static void printVars() {
+        System.out.println(a);
+        System.out.println(b);
+    }
 }
