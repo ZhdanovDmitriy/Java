a7f8720fb58e01587a3b9c79417637e36ddc18b1
Дмитрий Жданов <D.Zhdanov66@ya.ru>
2025-11-18 12:47:28 +0300

    practice 21

--- practice/src/Main.java
+++ practice/src/Main.java
@@ -1,4 +1,38 @@
+public class Main {
+    //final-метод нельзя переопределить
+    static class Parent {
+        public final void doWork() {
+            System.out.println("Работа выполнена");
+        }
+    }
+    //попытка переопределить final-метод вызовет ошибку компиляции
+    static class Child extends Parent {
+        // public void doWork() {
+        //     System.out.println("Попытка переопределить final-метод");
+        // }
+    }
+    /*
+     = = == = = = = = = = = == = Вторая часть практики = == = = = = = = =
+     */
 
-void main() {
+    //final-класс нельзя наследовать
+    final static class FinalClass {
+        public void action() {
+            System.out.println("Действие final-класса");
+        }
+    }
 
+    //ошибка компиляции:
+    //static class Subclass extends FinalClass { }
+
+    public static void main(String[] args) {
+        Parent p = new Parent();
+        p.doWork();
+
+        Child c = new Child();
+        c.doWork();  //вызывается версия из Parent(мы в этом уверены, т.к. написали final в методе doWork)
+
+        FinalClass f = new FinalClass();
+        f.action();
+    }
 }
