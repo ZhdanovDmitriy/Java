d3833c7b10379da9e3f403ef4a88498c55c9ace1
Дмитрий Жданов <D.Zhdanov66@ya.ru>
2025-11-19 20:50:17 +0300

    practice 27

--- practice/src/Main.java
+++ practice/src/Main.java
@@ -1,4 +1,31 @@
+public class Main {
+    //интерфейс с вложенным классом
+    interface Vehicle {
+        //вложенный класс по умолчанию является static
+        class Car {
+            private String brand;
 
-void main() {
+            public Car(String brand) {
+                this.brand = brand;
+            }
 
+            public void printBrand() {
+                System.out.println("Марка автомобиля: " + brand);
+            }
+        }
+
+        // Можно также определить статические методы внутри интерфейса
+        static void info() {
+            System.out.println("Это интерфейс Vehicle с вложенным классом Car.");
+        }
+    }
+
+    public static void main(String[] args) {
+        // Вызов статического метода интерфейса
+        Vehicle.info();
+        // Создание объекта вложенного класса
+        Vehicle.Car myCar = new Vehicle.Car("Toyota");
+        // Вызов метода объекта вложенного класса
+        myCar.printBrand();
+    }
 }
