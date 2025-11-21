cf8c20271e3b61477d765c9ec3732cc038eed5a0
Дмитрий Жданов <D.Zhdanov66@ya.ru>
2025-11-22 02:51:39 +0300

    practice 35

--- practice/src/Main.java
+++ practice/src/Main.java
@@ -1,4 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        //первое исключение будет перехвачено
+        try {
+            int a = 10;
+            int b = 0;
+            int c = a / b; //генерация ArithmeticException
+        } catch (ArithmeticException ex) {
+            System.out.println("Перехвачено исключение ArithmeticException");
+        }
 
-void main() {
+        System.out.println("Продолжение работы после первого исключения");
 
+        //второе исключение не перехватывается
+        //приводит к аварийному завершению
+        String text = null;
+        int len = text.length(); //генерация NullPointerException
+
+        //Exception in thread "main" java.lang.NullPointerException: Cannot invoke "String.length()" because "text" is null
+        //at Main.main(Main.java:17)
+        
+        System.out.println("Эта строка не выполнится");
+    }
 }
